78159b296466236c305f574ca752280d6f2d2068
courtlandcampo <courtland.campo@ufl.edu>
2022-07-01 10:07:11 -0400

    Question array created

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+var questionsArray = [
+    {
+        question: "Who is considered 'The People's Princess'?",
+        answer: 'Princess Diana',
+        options: ['Queen Elizabeth II', 'Princess Margaret', 'Kate Middleton', 'Princess Diana']
+    },
+
+    {
+        question: "Who portrayed Princess Diana in the 2021 film 'Spencer' (Larraín)?",
+        answer: 'Kristen Stewart',
+        options: ['Kristen Stewart', 'Natalie Portman', 'Florence Pugh', 'Elizabeth Debicki' ]
+    },
+
+    {
+        question: "In what year did Princess Diana marry Prince Charles?",
+        answer: '1981',
+        options: ['1983', '1979', '1981', '1992']
+    },
+
+    {
+        question: "Princess Diana held an auction of some of her dresses to raise money for what cause?",
+        answer: "AIDS and cancer charities",
+        options: ["AIDS and cancer charities", "Children displaced by war", "World hunger", "Parkinson's disease"]
+    },
+
+    {
+        question: "Where did Princess Diana die?",
+        answer: "Paris",
+        options: ["London", "Rome", "New York City", "Paris"]
+    }
+]
