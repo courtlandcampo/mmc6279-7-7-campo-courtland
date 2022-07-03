2d9fedd74788781d5d230cf9baab9cff42c58207
courtlandcampo <courtland.campo@ufl.edu>
2022-07-03 15:44:35 -0400

    Debugged numberCorrect

--- index.js
+++ index.js
@@ -57,33 +57,54 @@ var questionsArr = [
     }
 ]
 
-//START BUTTON
+
 var quizEl = document.querySelector('#quiz')
 var choices = document.getElementById("quiz").querySelectorAll("div button")
 var intervalId
-
-
-var startBtn = document.createElement("button")
-startBtn.setAttribute('id','start-quiz')
-startBtn.innerHTML = "Start Quiz"
-quizEl.appendChild(startBtn)
-
-
 var questionEl = document.createElement("p")
 var optionsDiv = document.createElement("div")
 var optionsBtn = document.createElement("button")
+var q = 0 //QUESTION TRACKER
+var numberCorrect = 0 //USER SCORE
 
-//QUESTION TRACKER
-var q = 0
 
-//USER SCORE
-numberCorrect = 0
+function gradeQuiz() { 
+    var score = Math.round((numberCorrect/questionsArr.length)*100)
+    quizEl.innerHTML = ""
+    quizEl.innerHTML = "Previous score: " + score +"%"
+    localStorage.setItem('previous-score', score)
 
-startBtn.onclick = function() {
-    quizEl.removeChild(startBtn) //REMOVE THE START BUTTON AFTER GAME BEGINS
-    newQuestion() //BRINGS IN NEW QUESTIONS
-    q++ //QUESTION TRACKER, IDK WHY IT WORKS, BUT IT DOES
+    q = 0
+    numberCorrect = 0
+    
+    startSetUp()
 }
+
+function startSetUp( ){
+    var startBtn = document.createElement("button")
+    startBtn.setAttribute('id','start-quiz')
+    startBtn.innerHTML = "Start Quiz"
+    quizEl.appendChild(startBtn)
+
+    startBtn.onclick = function() {
+        quizEl.removeChild(startBtn) //REMOVE THE START BUTTON AFTER GAME BEGINS
+        newQuestion() //BRINGS IN NEW QUESTIONS
+        q++ //QUESTION TRACKER, IDK WHY IT WORKS, BUT IT DOES
+    }
+}
+startSetUp()
+
+function startWithScore() {
+    var previousScore = localStorage.getItem('previous-score')
+    if (previousScore) {
+        var prevoiusScoreEl = document.createElement("p")
+        
+        quizEl.appendChild(prevoiusScoreEl)
+    } else {
+        startSetUp()
+    }
+}
+startWithScore()
 
 function newQuestion() {  
     quizEl.innerHTML = ""
@@ -100,6 +121,14 @@ function newQuestion() {
         optionsBtn.textContent = questionsArr[q].options[i]
 
         optionsDiv.appendChild(optionsBtn)
+
+        optionsBtn.addEventListener('click', function(e){
+            if (e.target.innerHTML === questionsArr[q].answer){
+                numberCorrect++
+            } else {
+                endOfRound()
+            }
+        })
     }
     quizEl.appendChild(optionsDiv)
 
@@ -112,23 +141,31 @@ function newQuestion() {
     //COUNTDOWN
     var intervalId = setInterval(function(){
         timerEl.textContent = Number(timerEl.textContent) - 1
-        if (timerEl.textContent === '-1') {
-            clearInterval(intervalId)
-            newQuestion()
-            q++    
+        if (timerEl.textContent === -1) {
+            endOfRound()
         }
     }, 1000)
 }
 
 optionsDiv.addEventListener('click', function(e){
-    if (e.target.innerHTML === questionsArr[q].answer){
-        clearInterval(intervalId)
+    if (e.target.textContent === questionsArr[q].answer){
         numberCorrect++
-        newQuestion()
+        endOfRound()
         q++
     } else {
-        clearInterval(intervalId)
-        newQuestion()
-        q++
+        endOfRound()
     }
+
 })
+
+function endOfRound(){
+    clearInterval(intervalId)
+    q++
+    if (q === questionsArr.length){
+        gradeQuiz()
+    } else {
+        newQuestion()
+    }
+}
+
+
